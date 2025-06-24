8cf968573e7bc487e321e53f49ef499b04187c7b
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2025-06-24 16:45:10 +0900

    Create todo.go

---
+++ todo.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Task struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+const dataFile = "todo.json"
+
+func loadTasks() ([]Task, error) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var tasks []Task
+	if err := json.NewDecoder(file).Decode(&tasks); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func saveTasks(tasks []Task) error {
+	file, err := os.Create(dataFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return json.NewEncoder(file).Encode(tasks)
+}
+
+func nextID(tasks []Task) int {
+	maxID := 0
+	for _, t := range tasks {
+		if t.ID > maxID {
+			maxID = t.ID
+		}
+	}
+	return maxID + 1
+}
+
+func AddTask(title string) {
+	panic("unimplemented")
+}
+
+func ListTasks() {
+	panic("unimplemented")
+}
+
+func CompleteTask(id int) {
+	panic("unimplemented")
+}
+
+func DeleteTask(id int) {
+	panic("unimplemented")
+}
